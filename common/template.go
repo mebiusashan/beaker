fb3314eae3e6724c676e54ee0e79dd4bff122d55
mebius <mcewei@qq.com>
2020-11-21 17:49:07 +0800

    add template const

---
+++ common/template.go
@@ -0,0 +1,13 @@
+package common
+
+const TEMPLATE_PAGE string = "post.jet"
+
+const TEMPLATE_ARCHIVE string = "post.jet"
+
+const TEMPLATE_HOME string = "home.jet"
+
+const TEMPLATE_CAT string = "home.jet"
+
+const TEMPLATE_TWEET string = "tweet.jet"
+
+const TEMPLATE_NotFound string = "404.jet"
