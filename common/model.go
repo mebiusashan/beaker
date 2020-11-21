2b3e414aa7f2c77584bc23ac505eb9264cc344b4
mebius <mcewei@qq.com>
2020-11-21 22:46:00 +0800

    change common model

--- common/model.go
+++ common/model.go
@@ -2,53 +2,26 @@ package common
 
 import "github.com/jinzhu/gorm"
 
-type BaseMsg struct {
-	Code int `json:"code"`
-	Data interface{}
-}
-
-type SuccMsg struct {
-	BaseMsg
-	Msg string `json:"msg"`
-}
-
-type LoginReq struct {
-	DK string
-	UN string
-	PW string
-}
-
-type TweList struct {
-	BaseMsg
-	CurPage   uint
-	TotlePage uint
-	TweNum    uint
-	List      []TweetDB
-}
-type TweetDB struct {
-	gorm.Model
-	Context string
-}
-
-type ArcDB struct {
+type ArticleModel struct {
 	gorm.Model
 	Catid   uint
 	Title   string
-	Context string
+	Content string
 }
 
-type CatDBDel struct {
-	CatDB
-	MvID uint
-}
-type CatDB struct {
-	gorm.Model
-	Cname string
-	Name  string
-}
-
-type PageDB struct {
+type PageModel struct {
 	gorm.Model
 	Title   string
-	Context string
+	Content string
+}
+
+type TweetModel struct {
+	gorm.Model
+	Content string
+}
+
+type CatModel struct {
+	gorm.Model
+	Name  string
+	Alias string
 }
