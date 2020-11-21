d17eb6bc63f53536ca3617960e3332984126a513
mebius <mcewei@qq.com>
2020-11-22 00:24:44 +0800

    open some const value

--- common/const.go
+++ common/const.go
@@ -11,28 +11,28 @@ const TAG_CAT string = "cat_"
 const TAG_NOTFOUND string = "404_"
 
 //数据库最大空闲数
-const def_Database_MAX_IDLE_NUM = 20
+const Def_Database_MAX_IDLE_NUM = 20
 
 //数据库最大连接数
-const def_Database_MAX_OPEN_NUM = 100
+const Def_Database_MAX_OPEN_NUM = 100
 
 //默认Redis过期时间
-const def_Redis_EXPIRE_TIME = 3600
+const Def_Redis_EXPIRE_TIME = 3600
 
 //默认网站名称
-const def_Website_SITE_NAME = "Beaker"
+const Def_Website_SITE_NAME = "Beaker"
 
 //默认网站描述
-const def_Website_SITE_DES = "Beaker is a simple blog system."
+const Def_Website_SITE_DES = "Beaker is a simple blog system."
 
 //默认footer内容
-const def_Website_SITE_FOOTER = "Beaker is a simple blog system."
+const Def_Website_SITE_FOOTER = "Beaker is a simple blog system."
 
 //默认首页显示记录数
-const def_Website_INDEX_LIST_NUM = 10
+const Def_Website_INDEX_LIST_NUM = 10
 
 //默认tweet每页显示数
-const def_Website_TWEET_NUM_ONE_PAGE = 10
+const Def_Website_TWEET_NUM_ONE_PAGE = 10
 
 const DefaultEditor string = "nano"
 
