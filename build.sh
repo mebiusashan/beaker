514cbc83cfab92571a7b205d880bc4b01d0ec4f3
mebius <mcewei@qq.com>
2020-11-22 22:35:44 +0800

    remove windows build

--- build.sh
+++ build.sh
@@ -17,7 +17,6 @@ func main(){
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o beaker_linux main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o beaker_mac main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o beaker_win main.go
 rm main.go
 
 # build linux server & admin
