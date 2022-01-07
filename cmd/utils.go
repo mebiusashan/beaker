4d3c94e0ceaaab26550c10afe3f9b4e1584a254a
mebiusashan <mcewei@qq.com>
2022-01-07 23:56:27 +0800

    fix windows newline CRLF to Unix LF

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"bytes"
+	"io"
 	"path"
 	"regexp"
+	"runtime"
 	"strings"
 
 	"github.com/asaskevich/govalidator"
@@ -24,7 +27,6 @@ func FindImageURL(markdown []byte) []common.ImgInfo {
 			}
 		}
 	}
-
 	return imgurls
 }
 
@@ -38,5 +40,73 @@ func convMarkdownImage(markdown []byte, mdPath string) (string, []common.ImgInfo
 			mdStr = strings.ReplaceAll(mdStr, imgPaths[i].Path, imgPaths[i].Md5+imgPaths[i].Suffix)
 		}
 	}
+
+	if runtime.GOOS == "windows" {
+		lfReader := DOS2Unix(strings.NewReader(mdStr))
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(lfReader)
+		mdStr = buf.String()
+	}
+
 	return mdStr, imgPaths
+}
+
+type byteReader struct {
+	io.Reader
+	buf [1]byte
+}
+
+func (b *byteReader) ReadByte() (byte, error) {
+	_, err := io.ReadFull(b.Reader, b.buf[:])
+	return b.buf[0], err
+}
+
+type dos2unix struct {
+	r    io.ByteReader
+	b    bool
+	char byte
+}
+
+func (d *dos2unix) Read(b []byte) (int, error) {
+	var n int
+	for len(b) > 0 {
+		if d.b {
+			b[0] = d.char
+			d.b = false
+			b = b[1:]
+			n++
+			continue
+		}
+		c, err := d.r.ReadByte()
+		if err != nil {
+			return n, err
+		}
+		if c == '\r' {
+			d.char, err = d.r.ReadByte()
+			if err != io.EOF {
+				if err != nil {
+					return n, err
+				}
+				if d.char == '\n' {
+					c = '\n'
+				} else {
+					d.b = true
+				}
+			}
+		}
+		b[0] = c
+		b = b[1:]
+		n++
+	}
+	return n, nil
+}
+
+// DOS2Unix wraps a byte reader with a reader that replaces all instances of
+// \r\n with \n
+func DOS2Unix(r io.Reader) io.Reader {
+	br, ok := r.(io.ByteReader)
+	if !ok {
+		br = &byteReader{Reader: r}
+	}
+	return &dos2unix{r: br}
 }
